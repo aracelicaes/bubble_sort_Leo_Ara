71299743b52c83e3330504b359aac5a0530fa897
Leonardo Mezu <leo.mez@outlook.com>
2019-12-04 16:00:36 -0500

    Second assignment for the project.

--- bin/bubble_sort.rb
+++ bin/bubble_sort.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 array = [4,3,78,2,0,2]
+arraytwo = [4,3,78,2,0,2,2,5,7852,54,8]
 
 def bubble_sort(array)
   go = array.length-1
@@ -26,5 +27,34 @@ def bubble_sort(array)
   return array
 end
 
-puts bubble_sort(array)
+
+
+
+def bubble_sort_by(array)
+  go = array.length-1
+
+  switchIt = true 
+
+  while switchIt
+
+  i = 0
+
+  switchIt = false
+		while i < go
+      if yield() > 0
+          array[i], array[i+1] = array[i+1], array[i]
+          switchIt = true
+      end
+
+      i += 1 #this is where we iterate & g2 next index
+    end
+
+  end
+
+  return array
+end
+
+puts bubble_sort_by(["hi","hello","hey"]) { |left, right| return left.length - right.length }
+
+
 
