b8db91ef312cc5d1cb39a878fc15197bac4cc6eb
Leonardo Mezu <leo.mez@outlook.com>
2019-12-04 16:43:58 -0500

    Edited for stickler

--- bin/bubble_sort.rb
+++ bin/bubble_sort.rb
@@ -1,62 +1,41 @@
 #!/usr/bin/env ruby
-array = [4,3,78,2,0,2]
-arraytwo = [4,3,78,2,0,2,2,5,7852,54,8]
+# frozen_string_literal: true
+
+array = [4, 3, 78, 2, 0, 2]
+arraytwo = [4, 3, 78, 2, 0, 2, 2, 5, 7852, 54, 8]
 
 def bubble_sort(array)
-  go = array.length-1
-
-  switchIt = true 
-
-  while switchIt
-
-  i = 0
-
-  switchIt = false
+  go = array.length - 1
+  switch_It = true
+  while switch_It
+    i = 0
+    switch_It = false
     while i < go
-        
-      if array[i] > array[i+1]
-          array[i], array[i+1] = array[i+1], array[i]
-          switchIt = true
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        switch_It = true
       end
-
-      i += 1 #this is where we iterate & g2 next index
+      i += 1 # this is where we iterate & g2 next index
     end
-
   end
-
-  return array
+  array
 end
-
-
-
 
 def bubble_sort_by(array)
-  go = array.length-1
-
-  switchIt = true 
-
-  while switchIt
-
-  i = 0
-
-  switchIt = false
-		while i < go
-      if yield(array[i], array [i+1]) > 0
-          array[i], array[i+1] = array[i+1], array[i]
-          switchIt = true
+  go = array.length - 1
+  switch_It = true
+  while switch_It
+    i = 0
+    switch_It = false
+    while i < go
+      if yield(array[i], array [i + 1]).positive?
+        array[i], array[i + 1] = array[i + 1], array[i]
+        switch_It = true
       end
-
       i += 1
-      
     end
-
   end
-
   array
-  
 end
 
-puts bubble_sort_by(["This is a big one","hi","hello","hey","hi there", "I'm", "Your friend"]) { |left, right| left.length - right.length }
-
-
-
+puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
